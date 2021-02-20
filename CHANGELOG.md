e043458375514bde6188b2b7dd6cb29d85093833
Benjamin Borbe <bborbe@rocketnews.de>
2021-02-20 21:11:34 +0100

    allow set address and port

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,11 @@ Please choose versions by [Semantic Versioning](http://semver.org/).
 * MINOR version when you add functionality in a backwards-compatible manner, and
 * PATCH version when you make backwards-compatible bug fixes.
 
+## 1.5.0
+
+* Allow set port and address
+* Rename ProxyName and ProxyPort ENV names
+
 ## 1.4.3
 
 * Use OpenJre 11
--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -10,13 +10,21 @@ ulimit -n 8192
 if [ "$1" = "/usr/local/bin/service.sh" ]; then
 	echo "configure ..."
 
-	HOSTNAME=${HOSTNAME:-"confluence.benjamin-borbe.de"}
-	echo "set hostname to ${HOSTNAME}"
-	sed -i "s/{{HOSTNAME}}/${HOSTNAME}/" /opt/confluence/conf/server.xml
-
-	PORT=${PORT:-"443"}
+	PORT=${PORT:-"8080"}
 	echo "set port to ${PORT}"
 	sed -i "s/{{PORT}}/${PORT}/" /opt/confluence/conf/server.xml
+
+	ADDRESS=${ADDRESS:-"0.0.0.0"}
+	echo "set address to ${ADDRESS}"
+	sed -i "s/{{ADDRESS}}/${ADDRESS}/" /opt/confluence/conf/server.xml
+
+	PROXY_NAME=${HOSTNAME:-"confluence.benjamin-borbe.de"}
+	echo "set proxyName to ${PROXY_NAME}"
+	sed -i "s/{{PROXY_NAME}}/${PROXY_NAME}/" /opt/confluence/conf/server.xml
+
+	PROXY_PORT=${PROXY_PORT:-"443"}
+	echo "set proxyPort to ${PROXY_PORT}"
+	sed -i "s/{{PROXY_PORT}}/${PROXY_PORT}/" /opt/confluence/conf/server.xml
 
 	PORT=${SCHEMA:-"https"}
 	echo "set schema to ${SCHEMA}"
